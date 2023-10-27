03ab3040d89dc4a95eae20d0f8d26690be8213d5
Ivan Augusto Xavier Souza <ivan@naesquina.com.br>
2023-10-27 18:53:55 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Exemplo de JavaScript simples
+document.addEventListener("DOMContentLoaded", function () {
+    // Este código será executado quando a página estiver completamente carregada
+    // Você pode adicionar suas interações JavaScript aqui
+
+    // Por exemplo, aqui estamos alterando o texto de um elemento com id "exemplo"
+    var exemploElemento = document.getElementById("exemplo");
+    if (exemploElemento) {
+        exemploElemento.textContent = "Este texto foi alterado por JavaScript!";
+    }
+});
